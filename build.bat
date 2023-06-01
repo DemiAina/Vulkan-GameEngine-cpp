8389fa661167f29af0a1f108389609f471763862
DemiAina <100835583+DemiAina@users.noreply.github.com>
2023-06-01 10:59:36 +0100

    build: Added build system

---
+++ build.bat
@@ -0,0 +1,11 @@
+@echo off
+
+call "C:\Program Files (x86)\Microsoft Visual Studio\2022\BuildTools\VC\Auxiliary\Build\vcvars64.bat"
+ 
+SET includes=/Isrc /I%VULKAN_SDK%/Include
+SET links=/link /LIBPATH:%VULKAN_SDK%/Lib vulkan-1.lib
+set defines=/D DEBUG
+
+echo "Building main.."
+
+cl /EHsc %includes% %defines% src/main.cpp %links%
