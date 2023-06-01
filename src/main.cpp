72816cd2e2ab101a0061b3a95ec3968c2ada4294
DemiAina <100835583+DemiAina@users.noreply.github.com>
2023-06-01 15:59:45 +0100

    Feat: Create Instance of vulkan

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,28 @@
+#include "vulkan/vulkan_core.h"
 #include <iostream>
-#include <vulkan/vulkan.h>
 
 int main(){
     
-    std::cout << "Hello World" << std::endl;
-        
+    VkApplicationInfo appInfo = {};
+    appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+    appInfo.pApplicationName = "Pong Game";
+    appInfo.pEngineName = "Demi Engine";
+
+    VkInstanceCreateInfo instanceInfo = {};
+    instanceInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+    instanceInfo.pApplicationInfo = &appInfo;
+    
+    
+    VkInstance instance;
+
+    VkResult result = vkCreateInstance(&instanceInfo, 0 , &instance);
+    
+    
+    if(result  == VK_SUCCESS){
+        std::cout << "Created instance successfully" << std::endl;
+    }
+
+
     return 0;
 
 }
